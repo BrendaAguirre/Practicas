1a36233d7697475e80c6768da16834fb436b6979
genaromendezl@gmail.com <Yomero2015>
2019-05-31 17:12:32 -0500

    Se establece comunicacion. Falta depurar

--- app/src/main/java/mx/edu/itver/tecnochat/ChatActivity.java
+++ app/src/main/java/mx/edu/itver/tecnochat/ChatActivity.java
@@ -5,34 +5,40 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import java.net.Socket;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 
 public class ChatActivity extends AppCompatActivity {
 
-    private ArrayList<String> listItems=new ArrayList<String>();
+    private ArrayList<String> listItems = new ArrayList<String>();
     private ArrayAdapter<String> adapter;
 
     private ListView chat;
-
-    private Socket socket;
+    private HiloMensajes tareaCliente;
 
     final Context context = this;
+
+    SimpleDateFormat formatterMDY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +50,7 @@ public class ChatActivity extends AppCompatActivity {
         String direccionIP = intent.getStringExtra(MainActivity.EXTRA_DIP);
         String usuario = intent.getStringExtra(MainActivity.EXTRA_USR);
 
+        formatterMDY = new SimpleDateFormat("MM/dd/yyyy HH:mm");
 
         listItems = new ArrayList<String>();
 
@@ -51,51 +58,127 @@ public class ChatActivity extends AppCompatActivity {
 
         adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, listItems);
 
-        // Here, you set the data in your ListView
+
         chat.setAdapter(adapter);
-/*
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        layoutManager.setStackFromEnd(true);
-        chat.setLayoutManager(layoutManager);
-*/
 
-        String response="";
+        String response = "";
 
-        Cliente miCliente = new Cliente(direccionIP, 3333, response, usuario);
-        miCliente.execute();
+        // Estas dos lineas se tienen que colocar para evitar un error de aviso de seguridad
+        // Esta aplicacion se tiene que convertir a una implementacion con AsyncTask
+        // Se deja así para que coincida con la implementación en la practica con Java Estandar
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
-        if (response.contains("Exception")){
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
-            alertDialogBuilder.setTitle("Error en la conexion");
+        tareaCliente = new HiloMensajes(this, direccionIP, 3333, usuario);
 
-            alertDialogBuilder
-                    .setMessage(response)
-                    .setCancelable(false)
-                    .setPositiveButton("OK",new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog,int id) {
-                            ChatActivity.this.finish();
-                        }
-                    });
-
-            // create alert dialog
-            AlertDialog alertDialog = alertDialogBuilder.create();
-
-            // show it
-            alertDialog.show();
-        }
+        tareaCliente.start();
 
         findViewById(R.id.btnEnviar).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 EditText edtMensaje = findViewById(R.id.edtMensaje);
-                listItems.add(edtMensaje.getText().toString());
-                adapter.notifyDataSetChanged();
+                String mensaje = edtMensaje.getText().toString();
+
+                addRespuesta(mensaje);
+
+                edtMensaje.onEditorAction(EditorInfo.IME_ACTION_DONE);
+
+                tareaCliente.enviar(mensaje);
             }
         });
 
     }
 
+    void addRespuesta(String respuesta) {
+        String fechaHora = formatterMDY.format(Calendar.getInstance().getTime());
+        listItems.add(fechaHora + " < " + respuesta);
+        adapter.notifyDataSetChanged();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        tareaCliente.salir = true;
+
+    }
+
+    public class HiloMensajes extends Thread {
+
+        Socket clientSocket;
+        PrintWriter out;
+        BufferedReader in;
+        String buffer;
+
+        String direccionIP = "";
+        int puerto = 0;
+        String usuario = "";
+
+        ChatActivity parent = null;
+
+        boolean salir = false;
+
+        HiloMensajes(ChatActivity _parent, String _direccionIP, int _puerto, String _usuario) {
+            parent = _parent;
+            direccionIP = _direccionIP;
+            puerto = _puerto;
+            usuario = _usuario;
+        }
+
+        public void run() {
+            String fechaHora;
+
+
+            try {
+                clientSocket = new Socket(direccionIP, puerto);
+
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+
+                buffer = in.readLine(); // Leer bienvenida
+
+                parent.addRespuesta(buffer);
+
+                out.println(usuario);
+
+                while (!this.salir) {
+                    buffer = in.readLine();
+
+                    if (buffer != null) {
+                        parent.addRespuesta(buffer);
+                    }
+                }
+            } catch (IOException ioe) {
+
+                Log.v("IOExcepcion:", ioe.getMessage());
+
+            } catch (Exception e) {
+
+                Log.v("Excepcion!:", e.getMessage());
+            }
+        }
+
+        void enviar(String mensaje) {
+            out.println(mensaje);
+        }
+
+        void cerrar() {
+            try {
+                clientSocket.close();
+                in.close();
+                out.close();
+                this.salir = true;
+            } catch (IOException ex) {
+                Log.v("error al cerrar", ex.getMessage());
+            }
+        }
+
+
+    }
 }
 
 /*
